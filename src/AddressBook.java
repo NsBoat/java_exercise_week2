6f7ad9aac0043869c26fe31c50921af244cca32d
NsBoat <apple@eduroam-int-dhcp-97-11-214.ucl.ac.uk>
2017-01-24 16:14:42 +0000

    FirstCommit
    First Commit

---
+++ src/AddressBook.java
@@ -0,0 +1,8 @@
+/**
+ * Created by apple on 1/24/17.
+ */
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<AddressBookEntry> emailAddress = new ArrayList<AddressBookEntry>();
+}
---
+++ src/AddressBookEntry.java
@@ -0,0 +1,21 @@
+/**
+ * Created by apple on 1/24/17.
+ */
+public class AddressBookEntry {
+
+    private String name;
+    private String phone;
+    private String email;
+
+    public AddressBookEntry(String name, String phone, String email){
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {return name;}
+
+    public String getPhone(){return phone;}
+
+    public String getEmail(){return email;}
+}
