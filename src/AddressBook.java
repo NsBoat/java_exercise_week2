04f82d726f56f65d3d89983d26c6139df5b85923
NsBoat <apple@eduroam-int-dhcp-98-209-184.ucl.ac.uk>
2017-01-25 02:20:38 +0000

    update 2nd class

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,5 +4,29 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    private ArrayList<AddressBookEntry> emailAddress = new ArrayList<AddressBookEntry>();
+    private ArrayList<AddressBookEntry> emailAddressBook;
+
+    public AddressBook(){
+        emailAddressBook = new ArrayList<AddressBookEntry>();
+    }
+
+    public void addAddress(AddressBookEntry entry){ emailAddressBook.add(entry); }
+
+    public String searchAddressByName(String name){
+        for (AddressBookEntry element : emailAddressBook){
+            if (element.getName() == name){
+                return element.getEmail();
+            }
+        }
+    }
+
+    private void removeEntry(String name){
+        private AddressBook i = new AddressBook();
+        for (AddressBookEntry element : emailAddressBook){
+            if (element.getName() == name){
+                emailAddressBook.remove(element);
+            }
+        }
+
+    }
 }
